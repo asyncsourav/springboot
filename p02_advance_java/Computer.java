bfdb6a3634ba433f4895aa073515705f888c6523
Sourav Kumar <xsouravkumar357@gmail.com>
2026-08-07 15:58:21 +0530

    Computer system

---
+++ p02_advance_java/Computer.java
@@ -0,0 +1,35 @@
+
+
+abstract class Comp {
+    abstract void Code();
+}
+
+class Laptop extends Comp {
+    public void Code() {
+        System.out.println("This is a working code");
+    }
+}
+
+class Desktop extends Comp {
+    public void Code() {
+        System.out.println("This is faster working code");
+    }
+}
+
+class Developer {
+    public void Work(Comp Device) {
+        Device.Code();
+    }
+}
+
+public class Computer {
+    public static void main(String[] args) {
+        
+        Laptop lap = new Laptop();
+        Desktop desk = new Desktop();
+
+        Developer dev = new Developer();
+        dev.Work(desk);
+        dev.Work(lap);
+    }
+}
