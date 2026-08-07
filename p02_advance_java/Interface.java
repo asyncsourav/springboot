10d212a34fed306bed07e16679f83b6ce7ea1ec7
Sourav Kumar <xsouravkumar357@gmail.com>
2026-08-07 15:39:43 +0530

    Interfaces - all methods are abstarct methods

---
+++ p02_advance_java/Interface.java
@@ -0,0 +1,34 @@
+
+
+interface Random {
+    void Show();
+    void Config();
+
+    // we cannot define any instance variable here - if we define then we have to also set it's value here only 
+    // int age;
+    // String name;
+    
+    // But we could define it - with it's value
+    int age = 54;
+    String name = "Sourav Kumar";
+
+    // But - even tough we define it here - we could not use these in any object.
+}
+
+public class Interface {
+    public static void main(String[] args) {
+        
+        Random r1 = new Random() {
+            public void Show() {
+                System.out.println("This is Aman Kumar Goswami");
+            }
+
+            public void Config() {
+                System.out.println("This is Nishwajit Gorai");
+            }
+        };
+
+        r1.Show();
+        r1.Config();
+    }
+}
