8ba9a1973818b3a68f4a0faa3fe9754ce692df84
Sourav Kumar <xsouravkumar357@gmail.com>
2026-08-11 09:45:00 +0530

    Dependency Injection

--- p06_p07_spring_demo/src/main/java/com/scalar/p06_p07_spring_demo/P06P07SpringDemoApplication.java
+++ p06_p07_spring_demo/src/main/java/com/scalar/p06_p07_spring_demo/P06P07SpringDemoApplication.java
@@ -1,15 +1,21 @@
 package com.scalar.p06_p07_spring_demo;
 
+import org.springframework.boot.ApplicationContextFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class P06P07SpringDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(P06P07SpringDemoApplication.class, args);
+		ApplicationContext context = SpringApplication.run(P06P07SpringDemoApplication.class, args);
 
-		System.out.println("hello world");
+		Alien abj = context.getBean(Alien.class);
+		abj.Code();
+
+		Alien obj1 = context.getBean(Alien.class);
+		obj1.Code();
 	}
 
 }
