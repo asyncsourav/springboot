ecbf6343d9092dfa520d13e983ad06982daf7453
Sourav Kumar <xsouravkumar357@gmail.com>
2026-08-11 09:35:07 +0530

    initialized project architecture by Spring initializr

---
+++ p06_p07_spring_demo/src/main/java/com/scalar/p06_p07_spring_demo/P06P07SpringDemoApplication.java
@@ -0,0 +1,15 @@
+package com.scalar.p06_p07_spring_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P06P07SpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P06P07SpringDemoApplication.class, args);
+
+		System.out.println("hello world");
+	}
+
+}
---
+++ p06_p07_spring_demo/src/test/java/com/scalar/p06_p07_spring_demo/P06P07SpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scalar.p06_p07_spring_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P06P07SpringDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
