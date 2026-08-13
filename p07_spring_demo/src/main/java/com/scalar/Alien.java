a11e6d896b9490d1fa7b8351b584f9ded792b972
Sourav Kumar <xsouravkumar357@gmail.com>
2026-08-14 01:26:52 +0530

    Run the first project by xml file auto Object creation

---
+++ p07_spring_demo/src/main/java/com/scalar/Alien.java
@@ -0,0 +1,11 @@
+package com.scalar;
+
+import org.springframework.stereotype.Component;
+
+
+public class Alien {
+
+    public void Code() {
+        System.out.println("Learning Coding here...");
+    }
+}
--- p07_spring_demo/src/main/java/com/scalar/App.java
+++ p07_spring_demo/src/main/java/com/scalar/App.java
@@ -2,11 +2,15 @@
 
 package com.scalar;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main( String[] args ) {
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+
+        Alien obj = (Alien) context.getBean("alien");
+        obj.Code();
     }
 }
