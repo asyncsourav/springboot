6fb112a2ed347e26cd466feeaca43dd305a6882d
Sourav Kumar <xsouravkumar357@gmail.com>
2026-08-14 22:05:46 +0530

    Add initial Spring JDBC project setup with Maven configuration and application files

---
+++ p11_spring_JDBC/src/main/java/com/scalar/p11_spring_JDBC/P11SpringJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.scalar.p11_spring_JDBC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P11SpringJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P11SpringJdbcApplication.class, args);
+		
+	}
+}
---
+++ p11_spring_JDBC/src/test/java/com/scalar/p11_spring_JDBC/P11SpringJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scalar.p11_spring_JDBC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P11SpringJdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
