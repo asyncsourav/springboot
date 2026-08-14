5563d609516554eecfce7ab6a33136961bef169a
Sourav Kumar <xsouravkumar357@gmail.com>
2026-08-14 23:17:44 +0530

    created temporary db in resources folder

--- p11_spring_JDBC/src/main/java/com/scalar/p11_spring_JDBC/repo/StudentRepo.java
+++ p11_spring_JDBC/src/main/java/com/scalar/p11_spring_JDBC/repo/StudentRepo.java
@@ -2,6 +2,8 @@ package com.scalar.p11_spring_JDBC.repo;
 
 
 import com.scalar.p11_spring_JDBC.model.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -11,8 +13,23 @@ import java.util.List;
 @Repository
 public class StudentRepo {
 
+    private JdbcTemplate jdbc;
+
+    public JdbcTemplate getJdbc() {
+        return jdbc;
+    }
+    @Autowired
+    public void setJdbc(JdbcTemplate jdbc) {
+        this.jdbc = jdbc;
+    }
+
     public void save(Student s) {
-        System.out.println("Data added");
+
+        String sql = "insert into student (rollno, name, marks) values(?, ?, ?)";
+        int rows = jdbc.update(sql, s.getRollNo(), s.getName(), s.getMarks());
+
+        System.out.println(rows + " row(s) effected");
+
     }
 
     public List<Student> findAll() {
