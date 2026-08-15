ca4cf30563c1907c6130381270d9d8147e51e22d
Sourav Kumar <xsouravkumar357@gmail.com>
2026-08-15 13:06:35 +0530

    basic template for serving a web page to the client for a route

---
+++ p12_spring_web/src/main/java/com/scalar/main/HomeController.java
@@ -0,0 +1,17 @@
+package com.scalar.main;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String Home() {
+        System.out.println("Home method called");
+        return "index.jsp";
+    }
+
+}
---
+++ p12_spring_web/src/main/java/com/scalar/main/MainApplication.java
@@ -0,0 +1,19 @@
+package com.scalar.main;
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class MainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainApplication.class, args);
+
+
+	}
+
+}
