4aba065590a6d2ff5f42697621feeb2b4a0f98de
Sourav Kumar <xsouravkumar357@gmail.com>
2026-08-15 13:39:46 +0530

    completed the entire MVC model here

--- p12_spring_web/src/main/java/com/scalar/main/HomeController.java
+++ p12_spring_web/src/main/java/com/scalar/main/HomeController.java
@@ -4,7 +4,9 @@ package com.scalar.main;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
@@ -13,19 +15,15 @@ public class HomeController {
     @RequestMapping("/")
     public String Home() {
         System.out.println("Home method called");
-        return "index.jsp";
+        return "index";
     }
 
     @RequestMapping("add")
-    public String Add (HttpServletRequest req, HttpSession session) {
-
-        int num1 = Integer.parseInt(req.getParameter("num1"));
-        int num2 = Integer.parseInt(req.getParameter("num2"));
+    public String Add (@RequestParam("num1") int num1, @RequestParam("num2") int num2, Model model) {
         int result = num1 + num2;
+        model.addAttribute("result", result);
 
-        session.setAttribute("result", result);
-
-        return "result.jsp";
+        return "result";
     }
 
 }
