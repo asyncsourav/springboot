0ddac9367b3732773e38b16082a37ff780a6df11
Sourav Kumar <xsouravkumar357@gmail.com>
2026-08-16 11:50:37 +0530

    removed unwanted dependency

--- p13_JobApp/src/main/java/com/telusko/JobApp/service/JobService.java
+++ p13_JobApp/src/main/java/com/telusko/JobApp/service/JobService.java
@@ -2,7 +2,6 @@ package com.telusko.JobApp.service;
 
 import com.telusko.JobApp.model.JobPost;
 import com.telusko.JobApp.repo.JobRepo;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
