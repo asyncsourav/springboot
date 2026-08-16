385b9cacfe366ad2bf0806a1f8068aaab8eac0b3
Sourav Kumar <xsouravkumar357@gmail.com>
2026-08-16 11:25:29 +0530

    updated the frontend

--- p17_eCommerce/client/src/main.jsx
+++ p17_eCommerce/client/src/main.jsx
@@ -1,9 +1,13 @@
+
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { useContext } from "react";
 import { AppProvider } from "./Context/Context.jsx";
+
+
 // import { BrowserRouter as Router } from "react-router-dom";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -14,3 +18,4 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     {/* </Router> */}
   </React.StrictMode>
 );
+
